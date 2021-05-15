872b00b1ecc74de818f7bb817b6279eac90901ea
RajashekharN <rajashekhar.narasaiah@gmail.com>
2021-05-15 20:51:55 +0530

    Next Commit

--- app/src/main/java/com/Android/weekendapp/MainActivity.java
+++ app/src/main/java/com/Android/weekendapp/MainActivity.java
@@ -3,12 +3,33 @@ package com.Android.weekendapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        LinearLayout.LayoutParams paramsRoot =
+                new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT);
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setLayoutParams(paramsRoot);
+
+        LinearLayout.LayoutParams paramsEdt = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+        EditText edtUserName = new EditText(this);
+        edtUserName.setLayoutParams(paramsEdt);
+        layout.addView(edtUserName);
+
+        LinearLayout.LayoutParams paramsBtn = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+        Button btnOK = new Button(this);
+        btnOK.setText("OK");
+        btnOK.setLayoutParams(paramsBtn);
+        layout.addView(btnOK);
+
+        setContentView(layout);
     }
 }
